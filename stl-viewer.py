613d2b69d5dd7464f0f68babb6e3da718fcb9fdb
wytr <mwtr@tuta.io>
2022-03-28 20:35:52 +0200

    test

--- stl-viewer.py
+++ stl-viewer.py
@@ -2,7 +2,9 @@ from random import randint
 from pyqtgraph.Qt import QtCore, QtGui
 import pyqtgraph as pg
 import pyqtgraph.opengl as gl
-
+import serial
+import math
+import json
 from OpenGL.GL import *
 from OpenGL.GL import shaders
 
@@ -14,7 +16,10 @@ import numpy as np
 from stl import mesh
 
 from pathlib import Path
-        
+
+ser = serial.Serial('COM4')
+ser.baudrate = 115200
+
 class MyWindow(QMainWindow):
 
     def __init__(self):
@@ -38,10 +43,7 @@ class MyWindow(QMainWindow):
         
         self.viewer = gl.GLViewWidget()
         layout.addWidget(self.viewer, 1)
-        
-        self.viewer.setWindowTitle('STL Viewer')
-        self.viewer.setCameraPosition(distance=40)
-        
+
         gx = gl.GLGridItem()
         gx.setSize(200, 200)
         gx.setSpacing(10, 10)
@@ -63,12 +65,41 @@ class MyWindow(QMainWindow):
         self.viewer.addItem(gy)
         self.viewer.addItem(gz)
         
+        self.pitch = 0
+        self.roll = 0
+
         btn = QPushButton(text="Load STL")
         btn.clicked.connect(self.showDialog)
         btn.clicked.connect(self.clicked)
         btn.setFont(QFont("Ricty Diminished", 14))
         layout.addWidget(btn)
 
+        self.graphWidget = pg.PlotWidget()
+        self.graphWidget.setYRange(-90, 90)
+        p1 = self.graphWidget.getPlotItem()
+        p1.hideAxis('bottom')
+
+        pen_x = pg.mkPen(color=(255, 0, 0), width=2)
+        pen_y = pg.mkPen(color=(0, 255, 0), width=2)
+        #pen_z = pg.mkPen(color=(0, 0, 255), width=2)
+
+        layout.addWidget(self.graphWidget)
+
+        self.pitch_plot_values = [0 for _ in range(100)] # 100 data points
+        self.roll_plot_values = [0 for _ in range(100)] # 100 data points
+
+        self.xAxis = list(range(len(self.pitch_plot_values)))  # 100 time points
+
+        self.xAccel_data_line = self.graphWidget.plot(
+            self.xAxis, self.pitch_plot_values, pen=pen_x)
+
+        self.yAccel_data_line = self.graphWidget.plot(
+            self.xAxis, self.roll_plot_values, pen=pen_y)
+            
+        self.viewer.setWindowTitle('STL Accelerometer Viewer')
+        self.viewer.setCameraPosition(distance=40)
+        
+        """
         self.slider_x = QSlider(Qt.Horizontal)
         self.slider_x.setMinimum(-180)
         self.slider_x.setMaximum(180)
@@ -92,23 +123,56 @@ class MyWindow(QMainWindow):
         self.slider_z.setTickPosition(QSlider.TicksBelow)
         self.slider_z.setTickInterval(1)
         layout.addWidget(self.slider_z)
-
-
-
+        """
         self.timer = QtCore.QTimer()
         self.time = QtCore.QTime(0, 0, 0)
         self.timer.timeout.connect(self.timerEvent)
+        self.timer.timeout.connect(self.update_plot_data)
         self.timer.start(10)
+
+    def update_plot_data(self):
+
+        ser_bytes = ser.readline()
+        decoded_bytes = ser_bytes[0:len(ser_bytes)-2].decode("utf-8")
+        jsonObject = json.loads(decoded_bytes)
+
+        self.pitch = self.calc_angle(
+            jsonObject["x-accel"], jsonObject["y-accel"], jsonObject["z-accel"], True)
+        self.roll = self.calc_angle(
+            jsonObject["x-accel"], jsonObject["y-accel"], jsonObject["z-accel"], False)
+
+        self.pitch_plot_values = self.pitch_plot_values[1:]
+        self.roll_plot_values = self.roll_plot_values[1:]
+
+        self.pitch_plot_values.append(self.pitch)
+        self.roll_plot_values.append(self.roll)
+        
+        self.xAccel_data_line.setData(self.xAxis, self.pitch_plot_values)
+        self.yAccel_data_line.setData(self.xAxis, self.roll_plot_values)
+        
+        if self.currentSTL:
+            self.currentSTL.resetTransform()
+            self.currentSTL.rotate(self.pitch,1,0,0)
+            self.currentSTL.rotate(self.roll,0,1,0)
+            self.currentSTL.rotate(0,0,0,1)
+            
+
+    def calc_angle(self, x, y, z, pitch):
+        # else return roll
+        if pitch:
+            return math.atan(x/(math.sqrt(z*z+y*y)))*180/math.pi
+        else:
+            return math.atan(y/(math.sqrt(z*z+x*x)))*180/math.pi
 
     def timerEvent(self):
         self.time = self.time.addSecs(1)
-        
+        """
         if self.currentSTL:
             self.currentSTL.resetTransform()
             self.currentSTL.rotate(self.slider_x.value(),1,0,0)
             self.currentSTL.rotate(self.slider_y.value(),0,1,0)
             self.currentSTL.rotate(self.slider_z.value(),0,0,1)
-
+        """
         print(self.time.toString("hh:mm:ss"))
 
     def clicked(self):
