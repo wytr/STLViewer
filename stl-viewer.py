7f3132b9879e9081ca90727b856dd109f6c22054
wytr <mwtr@tuta.io>
2022-03-28 02:43:49 +0200

    added more grids

--- stl-viewer.py
+++ stl-viewer.py
@@ -2,11 +2,14 @@ from random import randint
 from pyqtgraph.Qt import QtCore, QtGui
 import pyqtgraph as pg
 import pyqtgraph.opengl as gl
-import time
+
+from OpenGL.GL import *
+from OpenGL.GL import shaders
+
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *  
-import sys
+
 import numpy as np
 from stl import mesh
 
@@ -39,29 +42,73 @@ class MyWindow(QMainWindow):
         self.viewer.setWindowTitle('STL Viewer')
         self.viewer.setCameraPosition(distance=40)
         
-        g = gl.GLGridItem()
-        g.setSize(200, 200)
-        g.setSpacing(5, 5)
-        self.viewer.addItem(g)
+        gx = gl.GLGridItem()
+        gx.setSize(200, 200)
+        gx.setSpacing(10, 10)
+        gx.rotate(90, 0, 1, 0)
+        gx.translate(-100, 0, 0)
+
+        gy = gl.GLGridItem()
+        gy.setSize(200, 200)
+        gy.setSpacing(10, 10)
+        gy.rotate(90, 1, 0, 0)
+        gy.translate(0, -100, 0)
+
+        gz = gl.GLGridItem()
+        gz.setSize(200, 200)
+        gz.setSpacing(10, 10)
+        gz.translate(0, 0, -100)
+
+        self.viewer.addItem(gx)
+        self.viewer.addItem(gy)
+        self.viewer.addItem(gz)
         
         btn = QPushButton(text="Load STL")
         btn.clicked.connect(self.showDialog)
         btn.clicked.connect(self.clicked)
         btn.setFont(QFont("Ricty Diminished", 14))
-
         layout.addWidget(btn)
+
+        self.slider_x = QSlider(Qt.Horizontal)
+        self.slider_x.setMinimum(-180)
+        self.slider_x.setMaximum(180)
+        self.slider_x.setValue(0)
+        self.slider_x.setTickPosition(QSlider.TicksBelow)
+        self.slider_x.setTickInterval(1)
+        layout.addWidget(self.slider_x)
+
+        self.slider_y = QSlider(Qt.Horizontal)
+        self.slider_y.setMinimum(-180)
+        self.slider_y.setMaximum(180)
+        self.slider_y.setValue(0)
+        self.slider_y.setTickPosition(QSlider.TicksBelow)
+        self.slider_y.setTickInterval(1)
+        layout.addWidget(self.slider_y)
+
+        self.slider_z = QSlider(Qt.Horizontal)
+        self.slider_z.setMinimum(-180)
+        self.slider_z.setMaximum(180)
+        self.slider_z.setValue(0)
+        self.slider_z.setTickPosition(QSlider.TicksBelow)
+        self.slider_z.setTickInterval(1)
+        layout.addWidget(self.slider_z)
+
+
 
         self.timer = QtCore.QTimer()
         self.time = QtCore.QTime(0, 0, 0)
         self.timer.timeout.connect(self.timerEvent)
-        self.timer.start(100)
+        self.timer.start(10)
 
     def timerEvent(self):
         self.time = self.time.addSecs(1)
-
+        
         if self.currentSTL:
             self.currentSTL.resetTransform()
-            self.currentSTL.rotate(randint(0,100),0,1,0)
+            self.currentSTL.rotate(self.slider_x.value(),1,0,0)
+            self.currentSTL.rotate(self.slider_y.value(),0,1,0)
+            self.currentSTL.rotate(self.slider_z.value(),0,0,1)
+
         print(self.time.toString("hh:mm:ss"))
 
     def clicked(self):
@@ -85,7 +132,7 @@ class MyWindow(QMainWindow):
         points, faces = self.loadSTL(filename)
         meshdata = gl.MeshData(vertexes=points, faces=faces)
         mesh = gl.GLMeshItem(meshdata=meshdata, smooth=True, drawFaces=False, drawEdges=True, edgeColor=(0, 1, 0, 1))
-        
+        #mesh.setShader()
         self.viewer.addItem(mesh)
         
         self.currentSTL = mesh
