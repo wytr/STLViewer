2cbf153ae118bc921677f26b8902ef4a28fc6739
wytr <50141400+wytr@users.noreply.github.com>
2022-04-20 08:55:58 +0200

    implemented framecounter

--- glwidgettest.py
+++ glwidgettest.py
@@ -1,5 +1,5 @@
-
 import sys
+import time as pytime
 from OpenGL.GLU import *
 from OpenGL.GL import *
 from OpenGL.GL.shaders import compileProgram, compileShader
@@ -11,6 +11,9 @@ import pyrr
 from ObjLoader import ObjLoader
 from camera import Camera
 
+timeNow = 0
+
+
 class Ui_MainWindow(QtWidgets.QWidget):
 
     def __init__(self, parent=None):
@@ -19,9 +22,9 @@ class Ui_MainWindow(QtWidgets.QWidget):
         #self.button = QtWidgets.QPushButton('Test', self)
         mainLayout = QtWidgets.QHBoxLayout()
         mainLayout.addWidget(self.openGlWidget)
-        #mainLayout.addWidget(self.button)
+        # mainLayout.addWidget(self.button)
         self.setLayout(mainLayout)
-          
+
     def keyPressEvent(self, event):
         key = event.key()
 
@@ -63,7 +66,8 @@ class Ui_MainWindow(QtWidgets.QWidget):
         elif key == QtCore.Qt.Key.Key_D and not event.isAutoRepeat():
             print('D RELEASED')
             self.openGlWidget.right = False
-    
+
+
 class glWidget(QGLWidget):
 
     def __init__(self, parent=None):
@@ -109,13 +113,17 @@ class glWidget(QGLWidget):
         self.setMinimumSize(self.width, self.height)
         self.setCursor(QtCore.Qt.CursorShape.CrossCursor)
         self.setMouseTracking(True)
-        
-        self.cube_indices, self.cube_buffer = ObjLoader.load_model("meshes/cube.obj")
-        self.monkey_indices, self.monkey_buffer = ObjLoader.load_model("meshes/monkey.obj")
-        self.floor_indices, self.floor_buffer = ObjLoader.load_model("meshes/floor.obj")
 
+        self.cube_indices, self.cube_buffer = ObjLoader.load_model(
+            "meshes/cube.obj")
+        self.monkey_indices, self.monkey_buffer = ObjLoader.load_model(
+            "meshes/monkey.obj")
+        self.floor_indices, self.floor_buffer = ObjLoader.load_model(
+            "meshes/floor.obj")
+        self.frames = 0
+        self.fpscap = 60
         self.textures = None
-
+        self.useFrameCap = True
         self.shader = None
 
         self.VAO = None
@@ -145,7 +153,6 @@ class glWidget(QGLWidget):
     def set_first_mouse(self, b):
         self.first_mouse = b
 
-
     def do_movement(self):
         if self.left:
             self.cam.process_keyboard("LEFT", 0.05)
@@ -157,13 +164,14 @@ class glWidget(QGLWidget):
             self.cam.process_keyboard("BACKWARD", 0.05)
 
     def refreshViewport(self):
-            if self.initdone:
-                glViewport(0, 0, self.width, self.height)
-                projection = pyrr.matrix44.create_perspective_projection_matrix(45, self.width / self.height, 0.1, 100)
-                glUniformMatrix4fv(self.proj_loc, 1, GL_FALSE, projection)
-                 
+        if self.initdone:
+            glViewport(0, 0, self.width, self.height)
+            projection = pyrr.matrix44.create_perspective_projection_matrix(
+                45, self.width / self.height, 0.1, 100)
+            glUniformMatrix4fv(self.proj_loc, 1, GL_FALSE, projection)
+
     def resizeEvent(self, event):
-        
+
         QtWidgets.QWidget.resizeEvent(self, event)
         print("RESIZE")
         self.width = self.frameGeometry().width()
@@ -171,10 +179,9 @@ class glWidget(QGLWidget):
         print(self.width)
         print(self.height)
         self.refreshViewport()
-        
 
     def mouseMoveEvent(self, event):
-        
+
         xpos = event.x()
         ypos = event.y()
 
@@ -191,12 +198,16 @@ class glWidget(QGLWidget):
         lastY = ypos
 
         self.cam.process_mouse_movement(xoffset, yoffset)
-     
-    def paintGL(self):
-        
-        self.do_movement()
 
+    def paintGL(self):
+        global timeNow
+        self.do_movement()
         if self.initdone:
+
+            if(pytime.time() >= timeNow + 1):
+                timeNow += 1
+                print(self.frames)
+                self.frames = 0
 
             self.time = self.time.addMSecs(1)
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
@@ -214,31 +225,32 @@ class glWidget(QGLWidget):
             glUniformMatrix4fv(self.model_loc, 1, GL_FALSE, model)
             glDrawArrays(GL_TRIANGLES, 0, len(self.monkey_indices))
             """# draw the monkey
-            glBindVertexArray(self.VAO[1])
-            glBindTexture(GL_TEXTURE_2D, self.textures[1])
-            glUniformMatrix4fv(self.model_loc, 1, GL_FALSE, self.monkey_pos)
-            glDrawArrays(GL_TRIANGLES, 0, len(self.monkey_indices))
-            """
+                glBindVertexArray(self.VAO[1])
+                glBindTexture(GL_TEXTURE_2D, self.textures[1])
+                glUniformMatrix4fv(self.model_loc, 1, GL_FALSE, self.monkey_pos)
+                glDrawArrays(GL_TRIANGLES, 0, len(self.monkey_indices))
+                """
             # draw the floor
             glBindVertexArray(self.VAO[2])
             glBindTexture(GL_TEXTURE_2D, self.textures[2])
             glUniformMatrix4fv(self.model_loc, 1, GL_FALSE, self.floor_pos)
             glDrawArrays(GL_TRIANGLES, 0, len(self.floor_indices))
             self.update()
-
+            self.frames += 1
 
     def initializeGL(self):
         global initdone
         global vertex_src
         global fragment_src
 
-        self.shader = compileProgram(compileShader(self.vertex_src, GL_VERTEX_SHADER), compileShader(self.fragment_src, GL_FRAGMENT_SHADER))
-        
+        self.shader = compileProgram(compileShader(
+            self.vertex_src, GL_VERTEX_SHADER), compileShader(self.fragment_src, GL_FRAGMENT_SHADER))
+
         self.textures = glGenTextures(3)
         load_texture("meshes/crate.png", self.textures[0])
         load_texture("meshes/monkey.jpg", self.textures[1])
         load_texture("meshes/floor2.jpg", self.textures[2])
-        
+
         # VAO and VBO
         self.VAO = glGenVertexArrays(3)
         self.VBO = glGenBuffers(3)
@@ -246,66 +258,84 @@ class glWidget(QGLWidget):
         glBindVertexArray(self.VAO[0])
         # cube Vertex Buffer Object
         glBindBuffer(GL_ARRAY_BUFFER, self.VBO[0])
-        glBufferData(GL_ARRAY_BUFFER, self.cube_buffer.nbytes, self.cube_buffer, GL_STATIC_DRAW)
+        glBufferData(GL_ARRAY_BUFFER, self.cube_buffer.nbytes,
+                     self.cube_buffer, GL_STATIC_DRAW)
         # cube vertices
         glEnableVertexAttribArray(0)
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, self.cube_buffer.itemsize * 8, ctypes.c_void_p(0))
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE,
+                              self.cube_buffer.itemsize * 8, ctypes.c_void_p(0))
         # cube textures
         glEnableVertexAttribArray(1)
-        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, self.cube_buffer.itemsize * 8, ctypes.c_void_p(12))
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE,
+                              self.cube_buffer.itemsize * 8, ctypes.c_void_p(12))
         # cube normals
-        glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, self.cube_buffer.itemsize * 8, ctypes.c_void_p(20))
+        glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE,
+                              self.cube_buffer.itemsize * 8, ctypes.c_void_p(20))
         glEnableVertexAttribArray(2)
         # monkey VAO
         glBindVertexArray(self.VAO[1])
         # monkey Vertex Buffer Object
         glBindBuffer(GL_ARRAY_BUFFER, self.VBO[1])
-        glBufferData(GL_ARRAY_BUFFER, self.monkey_buffer.nbytes, self.monkey_buffer, GL_STATIC_DRAW)
+        glBufferData(GL_ARRAY_BUFFER, self.monkey_buffer.nbytes,
+                     self.monkey_buffer, GL_STATIC_DRAW)
         # monkey vertices
         glEnableVertexAttribArray(0)
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, self.monkey_buffer.itemsize * 8, ctypes.c_void_p(0))
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE,
+                              self.monkey_buffer.itemsize * 8, ctypes.c_void_p(0))
         # monkey textures
         glEnableVertexAttribArray(1)
-        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, self.monkey_buffer.itemsize * 8, ctypes.c_void_p(12))
+        glVertexAttribPointer(
+            1, 2, GL_FLOAT, GL_FALSE, self.monkey_buffer.itemsize * 8, ctypes.c_void_p(12))
         # monkey normals
-        glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, self.monkey_buffer.itemsize * 8, ctypes.c_void_p(20))
+        glVertexAttribPointer(
+            2, 3, GL_FLOAT, GL_FALSE, self.monkey_buffer.itemsize * 8, ctypes.c_void_p(20))
         glEnableVertexAttribArray(2)
         # floor VAO
         glBindVertexArray(self.VAO[2])
         # floor Vertex Buffer Object
         glBindBuffer(GL_ARRAY_BUFFER, self.VBO[2])
-        glBufferData(GL_ARRAY_BUFFER, self.floor_buffer.nbytes, self.floor_buffer, GL_STATIC_DRAW)
+        glBufferData(GL_ARRAY_BUFFER, self.floor_buffer.nbytes,
+                     self.floor_buffer, GL_STATIC_DRAW)
         # floor vertices
         glEnableVertexAttribArray(0)
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, self.floor_buffer.itemsize * 8, ctypes.c_void_p(0))
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE,
+                              self.floor_buffer.itemsize * 8, ctypes.c_void_p(0))
         # floor textures
         glEnableVertexAttribArray(1)
-        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, self.floor_buffer.itemsize * 8, ctypes.c_void_p(12))
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE,
+                              self.floor_buffer.itemsize * 8, ctypes.c_void_p(12))
         # floor normals
-        glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, self.floor_buffer.itemsize * 8, ctypes.c_void_p(20))
+        glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE,
+                              self.floor_buffer.itemsize * 8, ctypes.c_void_p(20))
         glEnableVertexAttribArray(2)
 
         glUseProgram(self.shader)
         glClearColor(0.1, 0.1, 0.1, 1)
         glEnable(GL_DEPTH_TEST)
-        
+
         glEnable(GL_BLEND)
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
-        self.projection = pyrr.matrix44.create_perspective_projection_matrix(120, self.width / self.height, 0.1, 1000)
-        self.cube_pos = pyrr.matrix44.create_from_translation(pyrr.Vector3([0, 4, -2]))
-        self.monkey_pos = pyrr.matrix44.create_from_translation(pyrr.Vector3([-4, 4, -4]))
-        self.floor_pos = pyrr.matrix44.create_from_translation(pyrr.Vector3([0, 0, 0]))
+        self.projection = pyrr.matrix44.create_perspective_projection_matrix(
+            120, self.width / self.height, 0.1, 1000)
+        self.cube_pos = pyrr.matrix44.create_from_translation(
+            pyrr.Vector3([0, 4, -2]))
+        self.monkey_pos = pyrr.matrix44.create_from_translation(
+            pyrr.Vector3([-4, 4, -4]))
+        self.floor_pos = pyrr.matrix44.create_from_translation(
+            pyrr.Vector3([0, 0, 0]))
         self.model_loc = glGetUniformLocation(self.shader, "model")
         self.proj_loc = glGetUniformLocation(self.shader, "projection")
         self.view_loc = glGetUniformLocation(self.shader, "view")
         glUniformMatrix4fv(self.proj_loc, 1, GL_FALSE, self.projection)
         self.initdone = True
         self.refreshViewport()
+        global timeNow
+        timeNow = pytime.time()
 
 
-if __name__ == '__main__':   
-    app = QtWidgets.QApplication(sys.argv)    
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
     Form = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow(Form)    
+    ui = Ui_MainWindow(Form)
     ui.show()
     sys.exit(app.exec_())
